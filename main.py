67bc7c366080934491a457901248689866de7761
leebit025 <kizianmoore@gmail.com>
2026-02-08 23:24:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ with open('./data.json') as f:
     data = json.load(f)
 
 
-@app.get('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.get('/stats')
+async def get_stats():
+    counts = {}
+    for student in data:
+        # Count programme
+        programme = student['programme']
+        if programme in counts:
+            counts[programme] += 1
+        else:
+            counts[programme] = 1
+        
+        # Count preference
+        pref = student['pref']
+        if pref in counts:
+            counts[pref] += 1
+        else:
+            counts[pref] = 1
+    
+    return counts
+    
