5ead369dbb700232b9f3a2d0ab4b8203f5173d89
leebit025 <kizianmoore@gmail.com>
2026-02-17 00:02:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,34 @@ with open('./data.json') as f:
 
 
 @app.get('/')
-def hello_world():
+async def hello_world():
     return 'Hello, World!'
+
+### New Function
+@app.get('/stats')
+async def get_stats():
+    counts = {}
+    for student in data:
+        # Count programme
+        programme = student['programme']
+        if programme in counts:
+            counts[programme] += 1
+        else:
+            counts[programme] = 1
+        
+        # Count preference
+        pref = student['pref']
+        if pref in counts:
+            counts[pref] += 1
+        else:
+            counts[pref] = 1
+    
+    return counts
+### End of new function
+
+a = 10 
+b = 4
+@app.get('/add/{a}/{b}')
+async def add(a: float, b: float):
+    result = a + b
+    return {"operation": "add", "a": a, "b": b, "result": result}
